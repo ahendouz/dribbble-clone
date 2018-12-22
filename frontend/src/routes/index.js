11cb52fbf72ec5d87735f4fa766d5567f27f5181
hicham ahendouz <hicham.ahendouz@imageplus.be>
2018-12-22 17:23:09 +0100

    update

--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -24,42 +24,34 @@ export const Root = ({ refetch, session }) => (
     <Fragment>
       <Navbar session={session} />
       <Switch>
-        <div>
-          <Route path="/" exact render={() => <Home session={session} />} />
+        <Route path="/" exact render={() => <Home session={session} />} />
+        <Route
+          path="/signin"
+          render={() => <Signin refetch={refetch} session={session} />}
+        />
+        <Route
+          path="/signup"
+          render={() => <Signup refetch={refetch} session={session} />}
+        />
+        <Route path="/shot/add" render={() => <AddShot session={session} />} />
+        <Route path="/coming-soon" component={ComingSoon} />
+        {session && session.getCurrentUser && (
           <Route
-            path="/signin"
-            render={() => <Signin refetch={refetch} session={session} />}
+            path={`/${session.getCurrentUser.username}/likes`}
+            render={() => <UserFavoriteShots session={session} />}
           />
-          <Route
-            path="/signup"
-            render={() => <Signup refetch={refetch} session={session} />}
-          />
-          <Route
-            path="/shot/add"
-            render={() => <AddShot session={session} />}
-          />
-          <Route path="/coming-soon" component={ComingSoon} />
-          {session && session.getCurrentUser && (
-            <Route
-              path={`/${session.getCurrentUser.username}/likes`}
-              render={() => <UserFavoriteShots session={session} />}
-            />
-          )}
-          <Route
-            path="/shot/:id/edit"
-            render={() => <EditShot session={session} />}
-          />
-          <Route
-            path="/shot/:id"
-            render={() => <ShotPage session={session} />}
-          />
-          <Route
-            path="/:userId/edit"
-            render={() => <EditUser session={session} />}
-          />
-          <Route path="/:userId" render={() => <User session={session} />} />
-          <Redirect to="/" />
-        </div>
+        )}
+        <Route
+          path="/shot/:id/edit"
+          render={() => <EditShot session={session} />}
+        />
+        <Route path="/shot/:id" render={() => <ShotPage session={session} />} />
+        <Route
+          path="/:userId/edit"
+          render={() => <EditUser session={session} />}
+        />
+        <Route path="/:userId" render={() => <User session={session} />} />
+        <Redirect to="/" />
       </Switch>
       <Footer />
     </Fragment>
